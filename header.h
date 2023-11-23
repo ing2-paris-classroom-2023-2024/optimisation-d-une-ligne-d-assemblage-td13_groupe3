9d3af6b89d273abb6fab1018b82e8a847621f360
Aou31 <a.langot@orange.fr>
2023-11-23 14:20:39 +0100

    structure station et tache

--- header.h
+++ header.h
@@ -5,9 +5,12 @@
 #ifndef ECELECAR_HEADER_H
 #define ECELECAR_HEADER_H
 
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct sommet {
 
+    int temps_operation;
     int** exclusions; //  pointeur vers un pointeur
     int** precedences; // utilisé pour stocker l'adresse d'un tableau de pointeurs d'entiers
     int** operations;
@@ -15,6 +18,25 @@ typedef struct sommet {
     int nbr_total_precedences;
     int nbr_total_operations;
 
-};
+}t_sommet;
+
+typedef struct station{
+
+    int temps_operation;
+    int nbr_taches_total;
+    struct tache* tache;
+
+}t_station;
+
+typedef struct tache{
+
+    int identifiant;
+    int poids;
+    struct tache** taches_suivantes;
+    struct tache** taches_precedentes;
+    int nbr_total_taches_suivantes;
+    int nbr_total_taches_precedentes;
+
+}t_tache;
 
 #endif //ECELECAR_HEADER_H
