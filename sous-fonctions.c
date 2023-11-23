4bae2fca1aec9cf5d160686a202f2050c730c351
Aou31 <a.langot@orange.fr>
2023-11-23 18:29:08 +0100

    parametre_recuperer_info_sommet

--- sous-fonctions.c
+++ sous-fonctions.c
@@ -7,34 +7,32 @@ t_sommet recuperer__info_sommet(char* info_identifiant){
 
 
     // Chargement des données de précédence
-    strcpy(chaine_temporaire, "../fichiers_ressources/"); // ajoute le contenu du fichier dans la chaine temporaire
     strcat(chaine_temporaire, info_identifiant); // ajoute l'identifiant dans la chaine temporaire
+    strcpy(chaine_temporaire, "../fichiers_ressources/"); // ajoute le contenu du fichier dans la chaine temporaire
     strcat(chaine_temporaire, "/");
     strcat(chaine_temporaire, "precedences"); // ajoute le fait qu'il s'agit d'une précédence dans la chaine temporaire
     strcat(chaine_temporaire, ".txt");
-    info_sommet.precedences = remplir_sommet(); /** modifier les paramètres **/
+    info_sommet.precedences = remplir_sommet(chaine_temporaire, info_sommet.nbr_total_precedences, 0); /** modifier les paramètres **/
 
     // Chargement des données d'opération
-    strcpy(chaine_temporaire, "../fichiers_ressources/");
     strcat(chaine_temporaire, info_identifiant);
+    strcpy(chaine_temporaire, "../fichiers_ressources/");
     strcat(chaine_temporaire, "/");
     strcat(chaine_temporaire, "operations");
     strcat(chaine_temporaire, ".txt");
-    info_sommet.operations = remplir_sommet(); /** modifier les paramètres **/
+    info_sommet.operations = remplir_sommet(chaine_temporaire, info_sommet.nbr_total_operations, 0); /** modifier les paramètres **/
 
     // Chargement des données d'exclusions
-    strcpy(chaine_temporaire, "../fichiers_ressources/");
     strcat(chaine_temporaire, info_identifiant);
+    strcpy(chaine_temporaire, "../fichiers_ressources/");
     strcat(chaine_temporaire, "/");
     strcat(chaine_temporaire, "exclusions");
     strcat(chaine_temporaire, ".txt");
-    info_sommet.operations = remplir_sommet(); /** modifier les paramètres **/
-
-
+    info_sommet.operations = remplir_sommet(chaine_temporaire, info_sommet.nbr_total_exclusions, 1); /** modifier les paramètres **/
 
     // Chargement du temps de cycle
-    strcpy(chaine_temporaire, "../fichiers_ressources/");
     strcat(chaine_temporaire, info_identifiant);
+    strcpy(chaine_temporaire, "../fichiers_ressources/");
     strcat(chaine_temporaire, "/");
     strcat(chaine_temporaire, "temps_cycle");
     strcat(chaine_temporaire, ".txt");
