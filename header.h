a116c12415bbf085c2f9a8232f0b97bc74554dfe
noama <noam.aich@edu.ece.fr>
2023-11-26 21:11:56 +0100

    uptade header

--- header.h
+++ header.h
@@ -45,4 +45,68 @@ typedef struct tache{
 int** remplir_sommet();
 void afficher_sommet (t_sommet info_sommet);
 void liberer_memoire ( t_sommet info_sommet);
+
+
+
+
+
+// Espace des Bibliothèques :
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <limits.h>
+
+// **********  MACROS   **********
+
+
+
+
+// ********** STRUCTURE **********
+
+
+typedef struct DATAS {
+    int TCYCLE;         // Temps de Cycle
+    int** EXCLUSIONS;    // Données d'exclusion
+    int EXCLUSIONS_TOT;  // Nombre total d'exclusion
+    int** PRECEDENCES;   // Données de précédence
+    int PRECEDENCES_TOT; // Nombre total de precedence
+    int** OPERATIONS ;   // Données d'opération
+    int OPERATIONS_TOT;  // Nombre total d'operation
+} DATAS;
+
+
+typedef struct DATASET {
+    int T_CYCLE;
+    int TASK_TOT;
+    struct TASK* TASKS;
+} DATASET;
+typedef struct TASK;
+typedef struct TASK {
+    int BASEID;
+    int POIDS;
+    struct TASK** P;
+    int P_TOT;
+    struct TASK** E;
+    int E_TOT;
+} TASK;
+
+
+
+
+// **********  PROTOS   **********
+
+// Protos de Chargement de Données
+DATAS SCANDATAS(char* jeu_donnees);
+int** FILLDATAS(char* fname, int* tot, int cond);
+void DISPDATAS(DATAS datas);
+void FREEDATAS(DATAS datas);
+
+// Protos de l'Ordonnancement des Données
+DATASET DATASORT(DATAS datas);
+void DISPDATASET(DATASET dataset);
+void FREEDATASET(DATASET dataset);
+
+// Algorithme
+void ALGO(DATASET dataset);
 #endif //ECELECAR_HEADER_H
