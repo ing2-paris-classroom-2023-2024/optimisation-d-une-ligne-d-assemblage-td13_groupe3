5af263aa0b921e4d936d4e2c7a8d20d31a76dbb3
arnoguyet <arnaudguyet92@gmail.com>
2023-11-27 15:33:17 +0100

    main

--- main.c
+++ main.c
@@ -4,15 +4,5 @@ int main() {
     char* info_identifiant = "01";
     t_sommet sommet = recuperer_info_sommet(info_identifiant);
     afficher_sommet(sommet);
-    setbuf(stdout, 0);
-    char* jeu_donnees = "01";
-    DATAS datas = SCANDATAS(jeu_donnees);
-    DISPDATAS(datas);
-    DATASET dataset = DATASORT(datas);
-    DISPDATASET(dataset);
-
-    ALGO(dataset);
-
-    FREEDATASET(dataset);
     return 0;
 }
