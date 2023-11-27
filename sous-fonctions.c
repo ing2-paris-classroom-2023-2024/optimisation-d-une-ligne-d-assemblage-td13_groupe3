da8d4d3b0f9f5f092c56e3287a864dc6bf364f14
Aou31 <a.langot@orange.fr>
2023-11-27 12:35:28 +0100

    modif

--- sous-fonctions.c
+++ sous-fonctions.c
@@ -2,23 +2,25 @@
 
 t_sommet recuperer_info_sommet(char* info_identifiant) {
     t_sommet info_sommet;
-    char chaine_temporaire[100];
+    char* chaine_temporaire = malloc (100* sizeof(char));
 
     // Chargement des données de précédence
-    sprintf(chaine_temporaire, "../fichiers_ressources/%s/precedences.txt", info_identifiant);
+    sprintf(chaine_temporaire, "../fichiers_ressources/01/precedences.txt");
+    printf("%s", chaine_temporaire);
     info_sommet.precedences = remplir_sommet(chaine_temporaire, &(info_sommet.nbr_total_precedences), 0);
 
     // Chargement des données d'opération
-    sprintf(chaine_temporaire, "../fichiers_ressources/%s/operations.txt", info_identifiant);
-    info_sommet.operations = remplir_sommet(chaine_temporaire, &(info_sommet.nbr_total_operations), 0);
+    sprintf(chaine_temporaire, "../fichiers_ressources/01/operation.txt");
+    info_sommet.operations = remplir_sommet(chaine_temporaire, &(info_sommet.nbr_total_operations), 1);
 
     // Chargement des données d'exclusions
 
-    sprintf(chaine_temporaire, "../fichiers_ressources/%s/exclusions.txt", info_identifiant);
+    sprintf(chaine_temporaire, "../fichiers_ressources/01/exclusions.txt");
     info_sommet.exclusions = remplir_sommet(chaine_temporaire, &(info_sommet.nbr_total_exclusions), 1);
 
+
     // Chargement du temps de cycle
-    sprintf(chaine_temporaire, "../fichiers_ressources/%s/temps_cycle.txt", info_identifiant);
+    sprintf(chaine_temporaire, "../fichiers_ressources/01/temps_cycle.txt");
     FILE* fichier = fopen(chaine_temporaire, "r");
     if (fichier == NULL) {
         fprintf(stderr, "Erreur lors de l'ouverture du fichier %s\n", chaine_temporaire);
@@ -30,23 +32,30 @@ t_sommet recuperer_info_sommet(char* info_identifiant) {
         info_sommet.temps_operation *= 1000; // on multiplie par 1000 afin de ne pas avoir de float
     }
 
+
     fclose(fichier);
 
     return info_sommet;
 }
-
+/*
 int** remplir_sommet(char* nom_fichier, int* total, int condition) {
     FILE* fichier = fopen(nom_fichier, "r");
+
     if (fichier == NULL) {
         fprintf(stderr, "Erreur lors de l'ouverture du fichier %s\n", nom_fichier);
         exit(EXIT_FAILURE);
     }
 
+
+
     *total = 0;
-    int** tableau = NULL;
-    while (fscanf(fichier, "%d", &(tableau[*total][0])) != EOF) {
+    int** tableau = malloc(100* sizeof(int*));
+
+
+    while ( !feof(fichier) {
         tableau = realloc(tableau, (*total + 1) * sizeof(int*)); //  agrandit d'une ligne
         tableau[*total] = malloc(2 * sizeof(int)); // agrandit de deux colonnes
+
 
         if (condition == 1) {
             double variable_temporaire = 0.001;
@@ -56,11 +65,46 @@ int** remplir_sommet(char* nom_fichier, int* total, int condition) {
             fscanf(fichier, "%d", &(tableau[*total][1]));
         }
 
+
         (*total)++;
+
+
+
     }
+    printf("???");
+
+
+
+
 
     fclose(fichier);
     return tableau;
+}*/
+int** remplir_sommet(char* fname, int* tot, int cond){
+    FILE* fichier = fopen(fname, "rw+");
+    *tot = 0;
+
+
+    int** tab = (int**) malloc( sizeof(int*) );
+
+
+    while(!feof(fichier)){
+        tab = (int**) realloc( tab, (*tot+1)*sizeof(int*) );
+
+        tab[*tot] = (int*) malloc( 2*sizeof(int) );
+        if(cond) {
+            double temp = 0.001;
+            fscanf(fichier, "%d %lf\n", &(tab[*tot][0]), &(temp));
+            tab[*tot][1] = (int) (temp * 1000);
+        }
+        else{
+            fscanf(fichier, "%d %d\n", &(tab[*tot][0]), &(tab[*tot][1]));
+        }
+        (*tot)++;
+    }
+    fclose(fichier);
+    printf("\n");
+    return tab;
 }
 
 void afficher_sommet (t_sommet info_sommet){
@@ -102,7 +146,3 @@ void liberer_memoire ( t_sommet info_sommet){
     free(info_sommet.operations);
 }
 
-int main(){
-    char* info
-    
-}
